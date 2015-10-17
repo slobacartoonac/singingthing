03779d11d917a0078ac71246a0c4aae14971cbe3
markoni985 <marko.bubamare@gmail.com>
2015-10-17 20:53:42 +0200

    Added comments and sound label

--- finalGUI.py
+++ finalGUI.py
@@ -162,7 +162,6 @@ if __name__ == '__main__':
    camSet=camProperties()
    ccam=projection.cammera(w,h,camSet[0],camSet[1])
    ccam.set_position(camSet[2], camSet[3])
-   
    print h
    print w
    global runTk
@@ -173,28 +172,37 @@ if __name__ == '__main__':
    print 'GUI initialized...'
    var1=IntVar()
    var1.set('3')
+   freq = StringVar()
+   freq.set('0000')
    volume = IntVar()
    volume.set('0')
    frame= Frame(root, height=500, width=900)
    frame.grid(row=0, column=0)
    frame2= Frame(root, height=500, width=400)
    frame2.grid(row=0, column=1)
+   # Controll buttons
    tk.Button(frame2, text='CAPTURE', command=lambda: captureBg(queue), height=5, width=20).grid(row=0, column=0)
    tk.Button(frame2, text='DEBUG  ', command=lambda: debugMode(), height=5, width=20).grid(row=1, column=0, sticky = N)
    tk.Button(frame2, text='QUIT   ', command=lambda: quit_(root,p), height=5, width=20).grid(row=3, column=0, sticky = N)
+   # Mode buttons
    Radiobutton(frame2, text="Mode 1", variable=var1, value=1, indicatoron = 0,width = 14,padx = 20,).grid(row=4)#, sticky=W)  
    Radiobutton(frame2, text="Mode 2", variable=var1, value=2, indicatoron = 0,width = 14,padx = 20,).grid(row=5)#, sticky=W)
    Radiobutton(frame2, text="Mode 3", variable=var1, value=3, indicatoron = 0,width = 14,padx = 20,).grid(row=6)#, sticky=W)
    Radiobutton(frame2, text="Mode 4", variable=var1, value=4, indicatoron = 0,width = 14,padx = 20,).grid(row=7)#, sticky=W)
+   # Volume 
    tk.Label(frame2, text="Volume").grid(column=0, row=8)
    volumeSk = ttk.LabeledScale(frame2, from_=0, to=100,variable=volume)
    volumeSk.grid(column=0, row=9, sticky=('W', 'E'))
+   # Configure menu
    tk.Button(frame2, text='CONFIGURE   ', command=lambda: settingsWindow.settingsWindow(), height=2, width=20).grid(row=10, column=0, sticky = N)
+  # label for the video frame
    image_label = tk.Label(frame)#, height=480, width=640)#.grid(row=0)
-   sound_label = tk.Label(frame)
-   # label for the video frame
    image_label.pack(side=TOP)
+   # Sound freq indicator
+   sound_label = tk.Label(frame2, textvariable = freq)
+   sound_label.grid(column = 0, row =11)
    print 'GUI image label initialized...'
+   # Image capture thread
    p = threading.Thread(target=image_capture,args=(queue, runTk, source))
    p.start()
    print 'image capture process has started...'
