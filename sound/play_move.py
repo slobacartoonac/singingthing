ba055e4a50e7af91c59b2b71cef1e36c971a065c
slobacartoonac <slobacartoonac@gmail.com>
2015-10-17 19:41:08 +0200

    Fix problem with sound when no detected objects

--- sound/play_move.py
+++ sound/play_move.py
@@ -32,51 +32,68 @@ def updateSound(dist,difference):
     print "set F: "+str(f)
     que.put(f)
 def updateMotion(inn):
-        print "updateMotion"
-        print inn
+        #print "updateMotion"
+        #print inn,' ',inn[2]!=-1
+        middle=False
         if(inn[2]!=-1):
+            #print "regular: ",inn
             buf.append(inn)
         else:
-            buf.append(buf[len(buf)-1])
+            middle=True
         while len(buf)>6:
             buf.pop(0)
         if(len(buf)<3):
             return
         s1=[sum(x) for x in zip(*buf)]
-        time=s1[2]/len(buf)
-        print "time: ",time," ",len(buf)
+        time=float(s1[2]/len(buf))
+        #print "averafe time: ",time
+        #print "time: ",time," ",len(buf)
         last=len(buf)-1
+        #print "a"
+        #print buf
         s2=((s1[0]-buf[0][0])/last,(s1[1]-buf[0][1])/last)
-        
+        if middle:
+            buf.append((s2[0],s2[1],time*2));
+        #print "b"
         s1=((s1[1] - buf[last][0])/last ,(s1[1] - buf[last][1])/last)
-        updateSound(distance(s1,s2),time)
+        #print "c ", s1,s2
+        dist=distance(s1,s2)
+        #print "d"
+       # print "distance/time: ",dist, time
+        updateSound(dist,time)
     
 def motionE(event):
     x, y = event.x, event.y
     global last,lastp
     if(time.time()-last)>0.1:
-        print('{}, {}'.format(x, y))
+        #print('{}, {}'.format(x, y))
         diffrence=time.time()-last
         last=time.time()
         if x==0 and y==0:
+            #print "nothing"
             updateMotion((0,0,-1))
+            return
         ny=cam.get_distance_pixel(ycord-y)
         nx=cam.get_cord_pixel(ny,x)
-        print('{}, {}'.format(int(nx), int(ny)))
+        #print('{}, {}'.format(int(nx), int(ny)))
         updateMotion((nx,ny,diffrence))
 def motion(event,cam):
-    print "usaoUmoution"
+    #print "usaoUmoution"
     x, y = event[0],event[1]
     global last,lastp
     if(time.time()-last)>0.1:
-        print('{}, {}'.format(x, y))
+        #print('{}, {}'.format(x, y))
         diffrence=time.time()-last
         last=time.time()
-        print "pre"
+        if event==(0,0):
+            #print "nista nista nista"
+            updateMotion((0,0,-1))
+            return
+        #print "pre"
         ny=cam.get_distance_pixel(cam.res_y-y)
-        print "posle"
+        #print "posle"
         nx=cam.get_cord_pixel(ny,x)
-        print('{}, {}'.format(int(nx), int(ny)))
+        #print('{}, {}'.format(int(nx), int(ny)))
         updateMotion((nx,ny,diffrence))
 def end():
     que.put(-1)
